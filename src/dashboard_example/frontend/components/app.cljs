24d25b2de3b33c4612f65191c6c73ff96b7f0556
Daniel Higginbotham <daniel@flyingmachinestudios.com>
2019-03-24 22:15:50 -0400

    a little more progress with filtering

--- src/dashboard_example/frontend/components/app.cljs
+++ src/dashboard_example/frontend/components/app.cljs
@@ -1,20 +1,41 @@
 (ns dashboard-example.frontend.components.app
   (:require [re-frame.core :as rf]
-            [dashboard-example.frontend.subs :as ds]))
+            [dashboard-example.frontend.subs :as ds]
+            [sweet-tooth.frontend.form.flow :as stff]
+            [sweet-tooth.frontend.form.components :as stfc]))
 
 (def fields
   [:movie/year :movie/imdb :movie/title :movie/test :movie/clean-test
    :movie/binary :movie/budget :movie/domgross :movie/intgross
    :movie/budget-2013 :movie/domgross-2013 :movie/intgross-2013])
 
+(defn report
+  []
+  (let [form-path                  [:report :create]
+        {:keys [field form-state]} (stfc/form form-path)]
+    [:form.create-report (stfc/on-submit form-path)
+     [field :text :report/title]
+     [:div
+      [field :number :report/budget-min]
+      [field :number :report/budget-max]]
+     [:div
+      [field :number :report/domgross-min]
+      [field :number :report/domgross-max]]
+
+     [:div
+      [field :number :report/intgross-min]
+      [field :number :report/intgross-max]]]))
+
 (defn row
   [movie]
   (into ^{:key (:db/id movie)} [:tr]
-        (map (fn [field] [:td (field movie)]) fields)))
+        (map (fn [field] [:td (str (field movie))]) fields)))
 
 (defn app
   []
-  [:div "APP!"
-   [:table
-    [:tbody
-     (map row @(rf/subscribe [::ds/movies]))]]])
+  (let [movies @(rf/subscribe [::ds/filtered-movies])]
+    [:div 
+     [report]
+     [:div (str "showing " (count movies) " movies")]
+     [:table
+      [:tbody (map row movies)]]]))
--- src/dashboard_example/frontend/subs.cljs
+++ src/dashboard_example/frontend/subs.cljs
@@ -1,8 +1,24 @@
 (ns dashboard-example.frontend.subs
-  (:require [re-frame.core :as rf]))
+  (:require [re-frame.core :as rf]
+            [sweet-tooth.frontend.filter.flow :as stfilter]))
 
 (rf/reg-sub ::movies
   (fn [db _]
     (->> (get-in db [:entity :movie])
          (vals)
          (sort-by :movie/title))))
+
+
+(stfilter/reg-filtered-sub
+  ::filtered-movies
+  ::movies
+  [:report :create]
+  [[:report/title stfilter/filter-query :movie/title]
+   [:report/budget-min stfilter/filter-attr>= :movie/budget]
+   [:report/budget-max stfilter/filter-attr<= :movie/budget]
+
+   [:report/domgross-min stfilter/filter-attr>= :movie/domgross]
+   [:report/domgross-max stfilter/filter-attr<= :movie/domgross]
+
+   [:report/intgross-min stfilter/filter-attr>= :movie/intgross]
+   [:report/intgross-max stfilter/filter-attr<= :movie/intgross]])
